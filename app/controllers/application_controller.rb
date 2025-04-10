d103aaaa8a00a1a96ddc184397d28495c8941577
Bernard Banta <banta.bernard@gmail.com>
2025-04-10 13:57:49 +0300

    Add contact us and more info pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,12 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  get "/more_info" do
+    erb :more_info
+  end
+
+  get "/contact_us" do
+    erb :contact_us
+  end
 end
