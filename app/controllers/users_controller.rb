f64fe5f52ded4bcd99b8df39b2d407f0adcf334e
Bernard Banta <banta.bernard@gmail.com>
2025-04-11 05:36:11 +0300

    Create login and register pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  configure do
+    set :views, 'app/views/users'
+  end
+
+  get "/register" do
+    erb :register
+  end
+
+  get "/login" do
+    erb :login
+  end
 end
