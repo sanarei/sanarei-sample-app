4c8885e77289997353d889a64136e41ffa81d63a
Bernard Banta <banta.bernard@gmail.com>
2025-04-11 05:01:03 +0300

    Change links for static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
+  configure do
+    set :views, 'app/views/static_pages'
+  end
+
   get "/" do
     erb :index
   end
 
-  get "/more_info" do
+  get "/static_pages/more_info" do
     erb :more_info
   end
 
-  get "/contact_us" do
+  get "/static_pages/contact_us" do
     erb :contact_us
   end
 end
