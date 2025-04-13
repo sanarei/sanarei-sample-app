65834200dcf624d6acb0c2797168fdebbbe8b67b
Bernard Banta <banta.bernard@gmail.com>
2025-04-13 21:43:25 +0300

    Added my_account page for users
    This is for displaying user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,30 @@ class UsersController < ApplicationController
   get "/login" do
     erb :login
   end
+
+  ##
+  # Displays the authenticated user's account details page.
+  #
+  # This route is protected by HTTP Basic Authentication using the `protected!` helper.
+  # The authenticated user is made available as `@current_user`, which can be accessed in the view.
+  #
+  # == Route
+  #   GET /users/my_account
+  #
+  # == Authentication
+  # - Requires valid HTTP Basic credentials.
+  # - If authentication fails, responds with 401 Unauthorized.
+  # - On success, renders the `:my_account` ERB view.
+  #
+  # == View Variables
+  # - +@current_user+:: The currently authenticated User object
+  #
+  # == Example curl request
+  #   curl -u 0712345678:1234 http://localhost:4000/users/my_account
+  #
+  # @return [String] Rendered HTML from the `:my_account` view
+  get '/my_account' do
+    protected!
+    erb :my_account
+  end
 end
