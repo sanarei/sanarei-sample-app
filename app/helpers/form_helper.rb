52ec29ba3c394fe7aacc731be193cf2149ae88b2
Bernard Banta <banta.bernard@gmail.com>
2025-04-14 04:27:46 +0300

    Update form helper to include permit_params method

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 ##
-# Module providing helpers for cleaning form input in Sinatra.
+# Module providing strong parameter filtering and cleaning for Sinatra forms.
 #
-# This is especially useful for ignoring blank fields in update forms,
-# so empty values don't overwrite existing user data unintentionally.
+# Includes methods to:
+# - Remove blank values from form inputs
+# - Permit only specified keys (like Rails strong params)
 #
 # == Usage
 #   require_relative 'form_helper'
 #   helpers FormHelper
 #
-#   post '/my_account/update' do
+#   patch '/users/update' do
 #     protected!
-#     @current_user.update_attributes(cleaned_params)
-#     redirect '/my_account'
+#     allowed_fields = %w[name phone_number national_id]
+#     @current_user.update_attributes(permit_params(*allowed_fields))
+#     redirect '/users/my_account'
 #   end
 #
 module FormHelper
@@ -32,5 +34,22 @@ module FormHelper
   # @return [Hash{String => String}] sanitized parameters with blank values removed
   def cleaned_params
     params.reject { |_, value| value.to_s.strip.empty? }
+  end
+
+  ##
+  # Filters incoming +params+ to only allow specified keys.
+  #
+  # This mimics Rails' strong parameters, allowing you to safely whitelist fields.
+  # Blank values are also automatically removed.
+  #
+  # == Example
+  #   # Given params: { "name" => "Alice", "_method" => "patch", "pin" => "" }
+  #   permit_params("name", "pin")
+  #   # => { "name" => "Alice" }
+  #
+  # @param [Array<String>] allowed_keys keys to allow through
+  # @return [Hash{String => String}] filtered params
+  def permit_params(*allowed_keys)
+    cleaned_params.select { |key, _| allowed_keys.include?(key) }
   end
 end
